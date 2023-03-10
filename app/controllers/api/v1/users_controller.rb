61963a04c8227c1e071f28cf61f92856e3b68684
Marco Antonio Hincapie Montes <marcohincapiem@gmail.com>
2023-03-10 12:33:15 -0500

    Add users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,57 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[ show update destroy ]
+
+  # GET /api/v1/users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /api/v1/users/1
+  def show
+    render json: @user
+  end
+
+  # POST /api/v1/users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.fetch(:user, {})
+  end
+
+  # Only allow a list of trusted JSON parameters through.
+  def json_params
+    allowed_data = %(email password description).freeze
+    json_payload.select { |allow| allowed_data.include?(allow) }
+  end
+end
