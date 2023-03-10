fef2cfc8d41509bf45633cf7d703b9d602757424
Marco Antonio Hincapie Montes <marcohincapiem@gmail.com>
2023-03-10 12:56:15 -0500

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authorize_request, except: %i[create index]
   before_action :set_user, only: %i[ show update destroy ]
 
   # GET /api/v1/users
@@ -15,7 +16,7 @@ class Api::V1::UsersController < ApplicationController
 
   # POST /api/v1/users
   def create
-    @user = User.new(user_params)
+    @user = User.new(json_params)
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -26,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
 
   # PATCH/PUT /api/v1/users/1
   def update
-    if @user.update(user_params)
+    if @user.update(json_params)
       render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -44,14 +45,9 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
-  def user_params
-    params.fetch(:user, {})
-  end
-
   # Only allow a list of trusted JSON parameters through.
   def json_params
-    allowed_data = %(email password description).freeze
+    allowed_data = %(email password name).freeze
     json_payload.select { |allow| allowed_data.include?(allow) }
   end
 end
