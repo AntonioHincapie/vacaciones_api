f0d0f6edeb65378689f113cf59d90a0378b7637b
Marco Antonio Hincapie Montes <marcohincapiem@gmail.com>
2023-03-11 13:43:37 -0500

    Condition seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+if User.count.zero?
+    User.create(name: "Marco", email: "marco@mail.com", password: "passwordM")
 
-User.create(name: "Marco", email: "marco@mail.com", password: "passwordM")
-
-Vacation.create(start_date: "2023-03-01", end_date: "2023-03-06", department: "IT", employee_name: "Andres Rodriguez")
-Vacation.create(start_date: "2023-02-01", end_date: "2023-02-06", department: "IT", employee_name: "Andres Rodriguez")
-Vacation.create(start_date: "2023-01-01", end_date: "2023-01-05", department: "IT", employee_name: "Andres Rodriguez")
-Vacation.create(start_date: "2022-12-01", end_date: "2022-12-05", department: "IT", employee_name: "Andres Rodriguez")
-Vacation.create(start_date: "2022-11-01", end_date: "2022-11-05", department: "IT", employee_name: "Andres Rodriguez")
-Vacation.create(start_date: "2021-02-25", end_date: "2023-03-02", department: "Marketing", employee_name: "Felipe Parra")
-Vacation.create(start_date: "2021-01-25", end_date: "2023-02-02", department: "Marketing", employee_name: "Felipe Parra")
-Vacation.create(start_date: "2022-12-25", end_date: "2023-01-02", department: "Marketing", employee_name: "Felipe Parra")
-Vacation.create(start_date: "2022-11-25", end_date: "2022-12-02", department: "Marketing", employee_name: "Felipe Parra")
-Vacation.create(start_date: "2022-10-25", end_date: "2022-11-02", department: "Marketing", employee_name: "Felipe Parra")
+    Vacation.create(start_date: "2023-03-01", end_date: "2023-03-06", department: "IT", employee_name: "Andres Rodriguez")
+    Vacation.create(start_date: "2023-02-01", end_date: "2023-02-06", department: "IT", employee_name: "Andres Rodriguez")
+    Vacation.create(start_date: "2023-01-01", end_date: "2023-01-05", department: "IT", employee_name: "Andres Rodriguez")
+    Vacation.create(start_date: "2022-12-01", end_date: "2022-12-05", department: "IT", employee_name: "Andres Rodriguez")
+    Vacation.create(start_date: "2022-11-01", end_date: "2022-11-05", department: "IT", employee_name: "Andres Rodriguez")
+    Vacation.create(start_date: "2021-02-25", end_date: "2023-03-02", department: "Marketing", employee_name: "Felipe Parra")
+    Vacation.create(start_date: "2021-01-25", end_date: "2023-02-02", department: "Marketing", employee_name: "Felipe Parra")
+    Vacation.create(start_date: "2022-12-25", end_date: "2023-01-02", department: "Marketing", employee_name: "Felipe Parra")
+    Vacation.create(start_date: "2022-11-25", end_date: "2022-12-02", department: "Marketing", employee_name: "Felipe Parra")
+    Vacation.create(start_date: "2022-10-25", end_date: "2022-11-02", department: "Marketing", employee_name: "Felipe Parra")
+end
